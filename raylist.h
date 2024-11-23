2aa4b8b6eb50cc61380388fc1d78aa4be01047c4
abdorayden <abdenoursouane025@gmail.com>
2024-11-23 11:32:24 +0100

    change version in licence

--- raylist.h
+++ raylist.h
@@ -1,5 +1,5 @@
 /************************************************************************************************
-*			Copyright (c) 2023 Ray Den	raylist v1.1.0				*
+*			Copyright (c) 2023 Ray Den	raylist v1.1.1				*
 *												*
 *	Permission is hereby granted, free of charge, to any person obtaining a copy		*
 *	of this software and associated documentation files (the "Software"), to deal		*
